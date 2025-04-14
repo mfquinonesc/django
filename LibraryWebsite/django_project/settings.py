e0d92533891ebea257b7ddb6da395435def9f194
mfquinonesc <manuelfqc@gmail.com>
2025-04-14 17:07:55 -0500

    [14/04/2025 17:05] update

--- LibraryWebsite/django_project/settings.py
+++ LibraryWebsite/django_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+    'rest_framework',
     # Local 
-    'books'
+    'books',
+    'apis'
 ]
 
 MIDDLEWARE = [
